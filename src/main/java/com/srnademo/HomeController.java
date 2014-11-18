7561e533cc930dcaef1f5229fdf9b88a201f61ce
akevinlee <klee@serena.com>
2014-11-18 17:40:51 +0000

    Turned into Web App

--- src/main/java/com/srnademo/HomeController.java
+++ src/main/java/com/srnademo/HomeController.java
@@ -1,21 +1,14 @@
 package com.srnademo;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@EnableAutoConfiguration
 public class HomeController {
 
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Welcome to Simple WebApp!";
-    }
-
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(HomeController.class, args);
+    @RequestMapping(value="/", method=RequestMethod.GET)
+    public String index() {
+        return "index";
     }
 }
