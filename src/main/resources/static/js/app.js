c4456b40c7f98041e34cc3ab31953b9ddc291b61
Kevin Lee <klee@serena.com>
2015-01-10 19:26:48 +0000

    Update yaml for mysql Corrected base URL in app.js

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,8 +1,8 @@
 var locationManagerModule = angular.module('locationManagerApp', ['ngAnimate']);
 
-locationManagerModule.controller('locationManagerController', function ($scope,$http) {
+locationManagerModule.controller('locationManagerController', function ($scope,$http,$location) {
 
-    var urlBase="";
+    var urlBase=getBaseUrl(); // TODO: replace with $location
     $scope.toggle=true;
     $http.defaults.headers.post["Content-Type"] = "application/json";
 
@@ -128,3 +128,22 @@ locationManagerModule.directive('ngConfirmClick', [
             }
         };
     }]);
+
+function getBaseUrl () {
+    var url = location.href;  // entire url including querystring - also: window.location.href;
+    var baseURL = url.substring(0, url.indexOf('/', 14));
+
+    if (baseURL.indexOf('http://localhost') != -1) {
+        // Base Url for localhost
+        var url = location.href;  // window.location.href;
+        var pathname = location.pathname;  // window.location.pathname;
+        var index1 = url.indexOf(pathname);
+        var index2 = url.indexOf("/", index1 + 1);
+        var baseLocalUrl = url.substr(0, index2);
+        return baseLocalUrl;
+    }
+    else {
+        // Root Url for domain name
+        return baseURL;
+    }
+};
